7d44da0a4dcad10af8b0975488cd884d875a66b8
Indra Puji Novirwan <indrapuji@gmail.com>
2020-10-15 21:43:31 +0700

    splash screen

---
+++ client/screens/SplashScreen.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import * as Animatable from "react-native-animatable";
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Animatable.Image animation="bounceInDown" source={require("../assets/logo.png")} style={styles.logo} resizeMode="stretch" />
+      </View>
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <Text style={styles.title}>Stay Update</Text>
+        <Text style={styles.text}>SignIn with Account</Text>
+        <View style={styles.button}>
+          <TouchableOpacity onPress={() => alert("click")}>
+            <LinearGradient colors={["#08d4c4", "#5fdde5"]} style={styles.signIn}>
+              <Text style={styles.textSign}>Next</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#009387",
+    backgroundColor: "#5fdde5",
+  },
+  header: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+  title: {
+    color: "#05375a",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "grey",
+    marginTop: 5,
+  },
+  button: {
+    alignItems: "flex-end",
+    marginTop: 30,
+  },
+  signIn: {
+    width: 150,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    flexDirection: "row",
+  },
+  textSign: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
