43bf19f7a79b0037c436b478ec76805716143bbe
Indra Puji Novirwan <indrapuji@gmail.com>
2020-12-30 21:10:06 +0700

    make it better

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const wishlistRouter = require("./wishlist");
-const UserController = require("../controllers/UsersController");
+const wishlistRouter = require('./wishlist');
+const UserController = require('../controllers/UsersController');
 
-router.get("/", (req, res) => res.json({ message: "Welcome to Todos Application" }));
+router.get('/', (req, res) => res.json({ message: 'movieApp' }));
 
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 
-router.use('/wishlist', wishlistRouter)
+router.use('/wishlist', wishlistRouter);
 
 module.exports = router;
